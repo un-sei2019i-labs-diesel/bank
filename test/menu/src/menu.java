e0747619e2843a72564e792287b652ead5b09470
fpieschaconr <fpieschaconr@unal.edu.co>
2019-05-21 15:55:27 -0500

    Test

---
+++ test/menu/src/menu.java
@@ -0,0 +1,48 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+ 
+public class menu {
+ 
+    public static void main(String[] args) {
+ 
+        Scanner sn = new Scanner(System.in);
+        boolean salir = false;
+        int opcion; //Guardaremos la opcion del usuario
+ 
+        while (!salir) {
+ 
+            System.out.println("1. Suma");
+            System.out.println("2. Resta");
+            System.out.println("3. Salir");
+ 
+            try {
+ 
+                System.out.println("Escribe una de las opciones");
+                opcion = sn.nextInt();
+ 
+                switch (opcion) {
+                    case 1:
+                        System.out.println("Suma");
+                        System.out.println("Primer entero");
+                        System.out.println("Segundo entero");
+                        break;
+                    case 2:
+                        System.out.println("Resta");
+                        System.out.println("Primer entero");
+                        System.out.println("Segundo entero");
+                        break;
+                    case 3:
+                        salir = true;
+                        break;
+                    default:
+                        System.out.println("Solo números entre 1 y 4");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Debes insertar un número");
+                sn.next();
+            }
+        }
+ 
+    }
+ 
+}
