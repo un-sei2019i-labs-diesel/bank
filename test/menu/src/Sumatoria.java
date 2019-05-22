0fd7dbeb38089d08bcf4b6e993fff872c6a23b5d
fpieschaconr <fpieschaconr@unal.edu.co>
2019-05-21 20:48:31 -0500

    merge feature2

---
+++ test/menu/src/Sumatoria.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Tom Perez
+ */
+
+public class Sumatoria {
+        public static int suma(int x, int y){
+            int z = x+y;
+            return z;
+        }
+    
+}
