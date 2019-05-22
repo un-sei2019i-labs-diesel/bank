ecb8d3751833a6b32debb22657bf16a13efb95cf
fpieschaconr <fpieschaconr@unal.edu.co>
2019-05-21 20:49:44 -0500

    merge feature3

---
+++ test/menu/src/Resta.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package resta;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Cesar
+ */
+public class Resta {
+        public static int resta(int min, int sust){
+            int respuesta = min-sust;
+            return respuesta;
+        }
+    
+}
